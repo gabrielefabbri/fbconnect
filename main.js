95cf2e0a4d22efb5b77ec7d22e30a6a4f53af466
Gabriele Fabbri <gabrielefabbri@gmail.com>
2021-10-30 20:51:49 +0200

    Jack img scale on scroll

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
 const gabriele = document.querySelector("h1");
+const jack = document.querySelector(".jack");
 
-
-
-let brcLeftPrev = gabriele.getBoundingClientRect().top;
-let i = 0;
 
 document.addEventListener("scroll", (evt) => {
   evt.preventDefault();
   var w = document.documentElement.scrollTop;
-  gabriele.style.transform = ("translateX(-" + w*0.30 + "px)");
+  gabriele.style.transform = ("translateX(-" + w * 0.30 + "px)");
+  
+  let scale = 1+(w/2000);
+  jack.style.transform = ("scale(" + scale + ")");
+  console.log(scale);
 });
